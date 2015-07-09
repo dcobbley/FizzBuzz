93178fcf381779bae2ef25f88770f7d2090e9ddc
Reynolds <reynoldd@por-reynoldd-m.ds.ad.adp.com>
2015-07-08 19:28:06 -0700

    This is our FizzBuzz impelementation = )

--- src/main/java/edu/pdx/CS410J/stuff/Project1.java
+++ src/main/java/edu/pdx/CS410J/stuff/Project1.java
@@ -8,12 +8,27 @@ import edu.pdx.cs410J.AbstractPhoneBill;
 public class Project1 {
 
   public static void main(String[] args) {
-    Class c = AbstractPhoneBill.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
-    for (String arg : args) {
-      System.out.println(arg);
+    for(int x = 1; x<=100;x++) {
+      //check if FizzBuzz - divisibly by 3 and 5
+      if(x%3 == 0 && x%5 == 0) {
+        System.out.println("FizzBuzz");
+      }
+      //check if Fizz - divisible by 3
+      else if (x%3 == 0) {
+        System.out.println("Fizz");
+      }
+      //check if Buzz - divisible by 5
+      else if (x%5 == 0) {
+        System.out.println("Buzz");
+      }
+      else {
+        System.out.println(x);
+      }
     }
-    System.exit(1);
+    System.exit(0);
   }
 
+  public static void julius() {
+    System.out.println("Good morning Julius...");
+  }
 }
