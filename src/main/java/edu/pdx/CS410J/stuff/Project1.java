6e31db0468e28e4af79e471220a38c2b069a6f16
Reynolds <reynoldd@por-reynoldd-m.ds.ad.adp.com>
2015-07-08 19:49:26 -0700

    Very minor changes

--- src/main/java/edu/pdx/CS410J/stuff/Project1.java
+++ src/main/java/edu/pdx/CS410J/stuff/Project1.java
@@ -10,27 +10,27 @@ import edu.pdx.cs410J.AbstractPhoneBill;
 public class Project1 {
 
   public static void main(String[] args) {
-    for(int x = 1; x<=100;x++) {
+    for(int not_x = 1; not_x<=100;not_x++) {
       //check if FizzBuzz - divisibly by 3 and 5
-      if(x%3 == 0 && x%5 == 0) {
+      if(not_x%3 == 0 && not_x%5 == 0) {
         System.out.println("FizzBuzz");
       }
       //check if Fizz - divisible by 3
-      else if (x%3 == 0) {
+      else if (not_x%3 == 0) {
         System.out.println("Fizz");
       }
       //check if Buzz - divisible by 5
-      else if (x%5 == 0) {
+      else if (not_x%5 == 0) {
         System.out.println("Buzz");
       }
       else {
-        System.out.println(x);
+        System.out.println(not_x);
       }
     }
     System.exit(0);
   }
 
   public static void julius() {
-    System.out.println("Good morning Julius...");
+    System.out.println("Good morning Julius... = )");
   }
 }
